69960197dde9bd8ad374693705c44c6f4512449f
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-03-03 11:02:13 +0100

    import module movies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { MoviesModule } from "./movies/movies.module";
 
 @NgModule({
   declarations: [
@@ -10,8 +11,9 @@ import { MoviesComponent } from './components/movies/movies.component';
     MoviesComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    MoviesModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
