2cdf0c6021a3fe44ea9cb8090b5c58022bcce62e
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-03-03 10:55:02 +0100

    create component movie module movies

---
+++ src/app/movies/components/movie/movie.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'movies-components-movie',
+  standalone: false,
+
+  templateUrl: './movie.component.html',
+  styleUrl: './movie.component.css',
+})
+export class MovieComponent {
+
+}
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
 import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { MovieComponent } from './components/movie/movie.component';
 
 
 
 @NgModule({
-  declarations: [MainPageComponent, MoviesListComponent],
+  declarations: [MainPageComponent, MoviesListComponent, MovieComponent],
   imports: [
     CommonModule
   ], 
