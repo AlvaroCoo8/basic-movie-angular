8b7fc565c21a4b8ee63f7d8d95165f7bace34588
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2025-03-05 23:36:24 +0100

    refactor delete component movie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MoviesComponent } from './components/movies/movies.component';
 import { MoviesModule } from "./movies/movies.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
   ],
   imports: [
     BrowserModule,
